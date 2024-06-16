8bbde805e71774ce41f1f0a9ba0726f13e161723
Rafaela Santos <rafaelasantos.informatica@gmail.com>
2024-06-16 11:45:53 -0400

    componente Message

--- lib/components/messages.dart
+++ lib/components/messages.dart
@@ -1,3 +1,4 @@
+import 'package:chat/core/services/chat/chat_service.dart';
 import 'package:flutter/material.dart';
 
 class Messages extends StatelessWidget {
@@ -5,8 +6,26 @@ class Messages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Messages'),
+    return StreamBuilder(
+      stream: ChatService().messsageStream(),
+      builder: (ctx, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+          return Center(
+            child: Text('Sem mensagens. Vamos iniciar?'),
+          );
+        } else {
+          final msgs = snapshot.data!;
+          return ListView.builder(
+            reverse: true,
+            itemCount: msgs.length,
+            itemBuilder: (ctx, i) => Text(msgs[i].text),
+          );
+        }
+      },
     );
   }
 }
